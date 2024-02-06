82edbca2e4d8394272c06772701f0ae425027a79
jeungHunLee <ksjs1111@gmail.com>
2024-02-06 14:18:36 +0900

    CHECKPASS-219 feat: Add logout button

--- CHECKPASS/Presentation/SeeMore/SeeMoreView.swift
+++ CHECKPASS/Presentation/SeeMore/SeeMoreView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct SeeMoreView: View {
+    @State private var showLogoutAlert: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section(header: Text("로그인 관리")) {
+                    Button(action: {
+                        showLogoutAlert.toggle()
+                    }, label: {
+                        Text("로그아웃")
+                            .foregroundColor(.red)
+                    })
+                    .alert("계정에서 로그아웃 하시겠어요?", isPresented: $showLogoutAlert) {
+                        Button(role: .destructive) {
+                            // Handle the deletion.
+                        } label: {
+                            Text("로그아웃")
+                        }
+                    }
+                }
+            }
+            .navigationTitle("더보기")
+            .navigationBarTitleDisplayMode(.large)
+            .listStyle(.plain)
+        }
     }
 }
 
